b50236117871a677d576b85b1b3bc94d2759324e
Term-inator <1121745627@qq.com>
2024-01-06 23:52:13 -0600

    scheduleService

--- src/schedule/urls.py
+++ src/schedule/urls.py
@@ -6,4 +6,18 @@ app_name = 'schedule'
 
 urlpatterns = [
     path('hello/', views.hello, name="hello"),
+    path('createSchedule/', views.createSchedule, name="createSchedule"),
+    path('updateScheduleById/', views.updateScheduleById, name="updateScheduleById"),
+    path('findEventsBetween/', views.findEventsBetween, name="findEventsBetween"),
+    path('findAllTodos/', views.findAllTodos, name="findAllTodos"),
+    path('findScheduleById/', views.findScheduleById, name="findScheduleById"),
+    path('findTimesByScheduleId/', views.findTimesByScheduleId, name="findTimesByScheduleId"),
+    path('findRecordsByScheduleId/', views.findRecordsByScheduleId, name="findRecordsByScheduleId"),
+    path('deleteScheduleById/', views.deleteScheduleById, name="deleteScheduleById"),
+    path('deleteTimeByIds/', views.deleteTimeByIds, name="deleteTimeByIds"),
+    path('updateTimeCommentById/', views.updateTimeCommentById, name="updateTimeCommentById"),
+    path('findAllSchedules/', views.findAllSchedules, name="findAllSchedules"),
+    path('updateDoneById/', views.updateDoneById, name='updateDoneById'),
+    path('updateStarById/', views.updateStarById, name='updateStarById'),
+    path('createRecord/', views.createRecord, name='createRecord'),
 ]
--- src/schedule/views.py
+++ src/schedule/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from .timeCodeParserTypes import DateUnit
+from . import service
 
 
 # Create your views here.
+def errorHandler(fn: callable):
+    def wrapper(*args, **kwargs):
+        try:
+            res = fn(*args, **kwargs)
+            return JsonResponse({'success': True, 'data': res})
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
+    return wrapper
 
 
 def hello(request):
@@ -12,4 +21,70 @@ def hello(request):
 
 
 def createSchedule(request):
-    pass
+    name, rTimeCode, comment, exTimeCode = request.POST['name'], request.POST['rTimeCode'], request.POST['comment'], request.POST['exTimeCode']
+    return errorHandler(service.createSchedule)(name, rTimeCode, comment, exTimeCode)
+
+
+def updateScheduleById(request):
+    id, name, rTimeCode, comment, exTimeCode = request.POST['id'], request.POST['name'], request.POST['rTimeCode'], request.POST['comment'], request.POST['exTimeCode']
+    return errorHandler(service.updateScheduleById)(id, name, rTimeCode, comment, exTimeCode)
+
+
+def findEventsBetween(request):
+    start, end = request.POST['start'], request.POST['end']
+    return errorHandler(service.findEventsBetween)(start, end)
+
+
+def findAllTodos(request):
+    return errorHandler(service.findAllTodos)()
+
+
+def findScheduleById(request):
+    id = request.POST['id']
+    return errorHandler(service.findScheduleById)(id)
+
+
+def findTimesByScheduleId(request):
+    scheduleId = request.POST['scheduleId']
+    return errorHandler(service.findTimesByScheduleId)(scheduleId)
+
+
+def findRecordsByScheduleId(request):
+    scheduleId = request.POST['scheduleId']
+    return errorHandler(service.findRecordsByScheduleId)(scheduleId)
+
+
+def deleteScheduleById(request):
+    id = request.POST['id']
+    return errorHandler(service.deleteScheduleById)(id)
+
+
+def deleteTimeByIds(request):
+    ids = request.POST['ids']
+    return errorHandler(service.deleteTimeByIds)(ids)
+
+
+def updateTimeCommentById(request):
+    id, comment = request.POST['id'], request.POST['comment']
+    return errorHandler(service.updateTimeCommentById)(id, comment)
+
+
+def findAllSchedules(request):
+    conditions, page, pageSize = request.POST['conditions'], request.POST['page'], request.POST['pageSize']
+    return errorHandler(service.findAllSchedules)()
+
+
+def updateDoneById(request):
+    id, done = request.POST['id'], request.POST['done']
+    return errorHandler(service.updateDoneById)(id, done)
+
+
+def updateStarById(request):
+    id, star = request.POST['id'], request.POST['star']
+    return errorHandler(service.updateStarById)(id, star)
+
+
+def createRecord(request):
+    scheduleId, startTime, endTime = request.POST['scheduleId'], request.POST['startTime'], request.POST['endTime']
+    return errorHandler(service.createRecord)(scheduleId, startTime, endTime)
+
