ae307e4962e39747441a603c9ead1eeaedfb7f38
Term-inator <1121745627@qq.com>
2024-02-04 21:02:48 +0000

    fix: 修复 isDev 判断异常

--- utils/env.py
+++ utils/env.py
@@ -2,11 +2,11 @@ import os
 
 
 def isDev():
-  return os.environ('DJANGO_SETTINGS_MODULE') == 'main.settings_dev'
+  return os.environ['DJANGO_SETTINGS_MODULE'] == 'main.settings_dev'
 
 
 def getHost():
-  if isDev:
+  if isDev():
     return 'http://127.0.0.1:8000'
   else:
     return 'https://sicongchen.top'
